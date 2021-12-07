9955f77b1f5d2cb959910b3bb1c737379ea7c33b
Emma <emmakanewoff@gmail.com>
2021-12-07 10:32:47 +0100

    Second draft

--- main.js
+++ main.js
@@ -1,109 +1,125 @@
-let parent = document.querySelector("#studentList");
+let noFilterStudentList = document.querySelector("#studentList");
+let filteredStudentList = document.querySelector("#filterStudentList");
+let filterHeadLine = document.querySelector("#filterHeadLine");
+let filterEducation = document.querySelector("#filterEducation");
+let choices = document.querySelectorAll("[name='student']");
 
-async function fetchInfo(){
-    let response = await fetch("https://api.mocki.io/v2/01047e91/students");
-    let students = await response.json();
-    //console.log(students);
+let sortButtons = document.querySelector("#sortButtons");
+let sortAge = document.querySelector("#sortAge");
+let sortFirstName = document.querySelector("#sortFirstName");
+let sortLastName = document.querySelector("#sortLastName");
 
-    let profileInfo = document.createElement("div");
-    students.forEach((object) => {
-        let profileName = document.createElement("p");
-        profileName.innerHTML = `${object.firstName} ${object.lastName}`;
-        profileInfo.appendChild(profileName);
-    }); 
-    parent.appendChild(profileInfo); 
+let filteredProgramme = "";
+
+sortButtons.hidden = true; //Knappar för att sortera ut elever döljs
+
+noFilterStudentList.hidden = false; //Lista med ofiltrerade elever visas
+
+async function fetchInfo() {
+  let response = await fetch("https://api.mocki.io/v2/01047e91/students");
+  let students = await response.json();
+  //console.log(students);
+
+  //Lista med alla elever
+  let profileInfo = document.createElement("div");
+  students.forEach((object) => {
+    let profileName = document.createElement("p");
+    profileName.innerHTML = `${object.firstName} ${object.lastName}, age: ${object.age}, programme: ${object.programme}`;
+    profileInfo.appendChild(profileName);
+  });
+  noFilterStudentList.appendChild(profileInfo);
+
+  //Knapp för att filtrera ut elever per utbildning
+  filterEducation.addEventListener("click", () => {
+    filteredStudentList.innerHTML = "";
+    choices.forEach((input) => {
+      if (input.checked) {
+        filteredProgramme = input.value;
+        noFilterStudentList.hidden = true; //Om en radiobutton är "checked", ofiltrerad lista göms
+      }
+    });
+
+    sortButtons.hidden = false;
+    //console.log(filteredProgramme);
+
+    let filter = students.filter(
+      (student) => student.programme === filteredProgramme
+    );
     
-    let filterEducation = document.querySelector("#filterEducation");
-    let choices = document.querySelectorAll("[name='student']");
+    //Skapar p-taggar för varje utfiltrerad elev
+    filter.forEach((student) => {
+      //console.log(student);
+      let eachStudent = document.createElement("p");
+      eachStudent.textContent = `${student.firstName} ${student.lastName}, ${student.age}`;
+      filteredStudentList.appendChild(eachStudent);   
+    });
 
-    filterEducation.addEventListener("click", () => {
-        let filteredStudents = "";
-        //id="studentlist"
-        choices.forEach(input => {
-            if (input.checked){
-                filteredStudents = input.value;
-            }
+    //Knapp för att sortera filtrerad lista i åldersordning med yngst först
+    sortAge.addEventListener("click", ()=> {
+        filter.sort((age1, age2) => {
+            return age1.age - age2.age;
         });
+
+        //Tömmer den existerande filtrerade listan
+        filteredStudentList.innerHTML = "";
         
-        let filteredList = [];
-        if (filteredStudents === "Frontend"){
-            filteredList = students.filter(item => item.programme === "Frontend");
-        } else if (filteredStudents === "Backend"){
-            filteredList = students.filter(item => item.programme === "Backend");
-        } else if (filteredStudents === "Net"){
-            filteredList = students.filter(item => item.programme === ".NET");
-        }
-
-        filteredList.forEach(item => {
-            let filteredStudents = document.createElement("p");
-            parent.appendChild(filteredStudents);
-            filteredStudents.innerHTML = `Name: ${item.firstName} ${item.lastName}, age: ${item.age}`;
+        //Skriver ut eleverna igen men i sorterad ordning
+        filter.forEach((student) => {
+            //console.log(student);
+            let eachStudent = document.createElement("p");
+            eachStudent.textContent = `${student.firstName} ${student.lastName}, ${student.age}`;
+            filteredStudentList.appendChild(eachStudent);   
+          });
+    });
+    
+    //Knapp för att sortera filtrerad lista i bokstavsordning baserat på förnamn
+    sortFirstName.addEventListener("click", ()=>{
+        // console.log("Sortera i bokstavsordning baserat på förnamn");
+        filter.sort((a, b)=>{
+            if (a.firstName < b.firstName) { 
+                return -1; 
+            } if (a.firstName > b.firstName) { 
+                return 1; 
+            }
+            return 0;
         });
-    })
 
-};
+        //Tömmer den existerande filtrerade listan
+        filteredStudentList.innerHTML = "";
+    
+        //Skriver ut eleverna igen men i sorterad ordning
+        filter.forEach((student) => {
+            //console.log(student);
+            let eachStudent = document.createElement("p");
+            eachStudent.textContent = `${student.firstName} ${student.lastName}, ${student.age}`;
+            filteredStudentList.appendChild(eachStudent);   
+        });
+    });
+    
+    //Knapp för att sortera filtrerad lista i bokstavsordning baserat på efternamn
+    sortLastName.addEventListener("click", ()=>{
+        //console.log("Sortera i bokstavsordning baserat på efternamn");
+        filter.sort((a, b)=>{
+            if (a.lastName < b.lastName) { 
+                return -1; 
+            } if (a.lastName > b.lastName) { 
+                return 1; 
+            }
+            return 0;
+        });
+
+        //Tömmer den existerande filtrerade listan
+        filteredStudentList.innerHTML = "";
+    
+        //Skriver ut eleverna igen men i sorterad ordning
+        filter.forEach((student) => {
+            //console.log(student);
+            let eachStudent = document.createElement("p");
+            eachStudent.textContent = `${student.firstName} ${student.lastName}, ${student.age}`;
+            filteredStudentList.appendChild(eachStudent);   
+        });
+    });
+  });
+}
 
 fetchInfo();
-
-//console.log(fetchInfo)
-
-
-
-
-//EXEMPELKOD FÖR ATT FILTRERA!!
-/*
-let button = document.querySelector("button");
-let inputs = document.querySelectorAll("[name='student']");
-let ulLista = document.querySelector("#ul")
-let checkbox = document.querySelector("#checkbox");
-
-button.addEventListener("click", () => {
-    if (checkbox.checked){
-        let showStudents = "";
-        ulLista.textContent = "";
-        inputs.forEach(input => {
-            if (input.checked){
-                showStudents = input.value;
-            }
-        });
-        let studentList = [];
-        if (showStudents === "Frontend") {
-            studentList = studentItems.filter(item => item.education === "Frontend");
-        } else if (showStudents === "Javascript") {
-            studentList = studentItems.filter(item => item.education === "Javascript");
-        } else if (showStudents === "UX") {
-            studentList = studentItems.filter(item => item.education === "UX");
-        }
-        
-        studentList.forEach(item => {
-            let filteredStudent = document.createElement("li");
-            ulLista.appendChild(filteredStudent);
-            filteredStudent.innerHTML = `${item.name}, <b>${item.city}</b>`;
-        });
-    } else {
-        ulLista.textContent = "Please confirm you’re a teacher!";
-    }
-});
-*/
-
-
-/*
-let newData = async (URL) => {
-let response = await fetch(URL);
-let data = await response.json();
-return data;
-};
-
-
-async function fetchInfo(){
-    let studenName = await newData("https://api.mocki.io/v2/01047e91/students");
-
-    studenName.forEach((object) => {
-        let profileName = document.createElement("p");
-        profileName.innerHTML = `<li>${object.firstName} ${object.lastName}</li>`;
-        document.querySelector("#studentList").appendChild(profileName)
-        //profileInfo.appendChild(profileName);
-    }); 
-    //parent.appendChild(profileInfo);   
-};
-*/
